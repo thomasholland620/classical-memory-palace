f31862a2437083726172acbad9888a0191d59722
thomasholland620 <thomas.holland620@gmail.com>
2025-03-26 13:29:12 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import postcss from './postcss.config.js';
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss
+  }
 });
