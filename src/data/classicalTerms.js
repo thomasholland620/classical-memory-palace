362c22a667546528aae2dd45fc1d7b23ee431479
thomasholland620 <thomas.holland620@gmail.com>
2025-03-26 10:32:01 +0000

    Add classicalTerms dataset

---
+++ src/data/classicalTerms.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    term: "Achilles",
+    location: "Thessaly",
+    mnemonic: "A heel-shaped shrine near Thessaly marks his weakness.",
+    category: "Myth"
+  },
+  {
+    term: "Plato",
+    location: "Athens",
+    mnemonic: "A flat stone (plato) table where dialogues echo in marble halls.",
+    category: "Philosophy"
+  },
+  {
+    term: "Julius Caesar",
+    location: "Rome",
+    mnemonic: "A crown atop a dagger balanced on a laurel in the Roman Forum.",
+    category: "History"
+  }
+];
+export default data;
